82e1b8ef2765e6ffbe45d51b1d71db4e5a0054f9
Alex Whiteside <1505496+alexw23@users.noreply.github.com>
2026-06-24 01:02:45 +0100

    Fixes

--- components/usb_webcam/usb_webcam.cpp
+++ components/usb_webcam/usb_webcam.cpp
@@ -185,7 +185,7 @@ esp_err_t esp_camera_init(USBWebCamFrameSize fs, uint32_t fps, uint32_t frame_bu
       .vs_format = {
           .h_res = frame_width,
           .v_res = frame_height,
-          .fps = 0, // let driver pick default fps from device descriptors
+          .fps = 10, // let driver pick default fps from device descriptors
           .format = UVC_VS_FORMAT_MJPEG,
       },
       .advanced = {
