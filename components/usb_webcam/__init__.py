064c0c46e45053f6df0d6828c62f6478b9583805
Alex Whiteside <1505496+alexw23@users.noreply.github.com>
2026-06-25 15:47:05 +0100

    Fixes

--- components/usb_webcam/__init__.py
+++ components/usb_webcam/__init__.py
@@ -144,10 +144,8 @@ async def to_code(config):
 
     # assert(CORE.is_esp_idf)
     add_idf_component(
-            name="usb_host_uvc",
-            repo="https://github.com/espressif/esp-usb.git",
-            path="host/class/uvc/usb_host_uvc",
-            ref="v2.5.1"
+        name="espressif/usb_host_uvc",
+        ref="2.5.1"
     )
     # no need in cg.add_library("espressif/esp32-camera", "1.0.0")
     # esp_camera.h and sensor.h are taken from it directly
