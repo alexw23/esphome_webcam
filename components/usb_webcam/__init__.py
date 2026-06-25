d729dbc99019483f475c4b9b0472226061c661f7
Alex Whiteside <1505496+alexw23@users.noreply.github.com>
2026-06-25 16:04:47 +0100

    Fixes

--- components/usb_webcam/__init__.py
+++ components/usb_webcam/__init__.py
@@ -145,10 +145,9 @@ async def to_code(config):
     # assert(CORE.is_esp_idf)
     add_idf_component(
             name="usb_host_uvc",
-            repo="https://github.com/espressif/esp-usb.git",
+            repo="https://github.com/alexw23/esp-usb.git",
             path="host/class/uvc/usb_host_uvc"
     )
-    cg.add_build_flag("-DCONFIG_UVC_CHECK_PAYLOAD_HEADER_ERR=0")
     # add_idf_component(
     #     name="espressif/usb_host_uvc",
     #     ref="2.5.1"
